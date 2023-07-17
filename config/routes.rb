d9df1c1a057f62c8c6390cd6c0a355cfa3c255f0
hoshiyuto <hoshi.yuto.infratop@gmail.com>
2023-07-17 04:51:50 +0000

    [Third]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#about"
-  resources :users, only: [:new, :index, :show, :create, :destroy]
-  resources :books, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:index, :show, :create, :edit, :update]
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
 
   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
