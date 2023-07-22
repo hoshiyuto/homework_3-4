858b9da835f503732a302d6a762a307161f53efa
hoshiyuto <hoshi.yuto.infratop@gmail.com>
2023-07-22 09:04:23 +0000

    [8th]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :users, only: [:index, :show, :create, :edit, :update]
   resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
-    resource :favorite, only: [:create, :destroy]
+    
   end
   
   get "/home/about" => "homes#about", as: "about"
