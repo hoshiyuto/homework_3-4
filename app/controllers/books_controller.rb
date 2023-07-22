0f358368b466849e56c18092bb48bf262fca8625
hoshiyuto <hoshi.yuto.infratop@gmail.com>
2023-07-22 07:04:50 +0000

    [7th]Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book2 = Book.new
+    @book_comment = BookComment.new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :users, only: [:index, :show, :create, :edit, :update]
-  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
-
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
+  end
+  
   get "/home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
