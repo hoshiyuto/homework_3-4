cf5b665f2c2c00c8e1bad99a29e01196ef93ddac
hoshiyuto <hoshi.yuto.infratop@gmail.com>
2023-07-24 10:42:43 +0000

    [Finish]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
-    
+
   end
-  
+
   get "/home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
